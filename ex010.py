3ac81e7d5533ff72d0aa75d02e7448d53a438c6c
abnermeroti <109527595+abnermeroti@users.noreply.github.com>
2022-10-10 14:17:40 -0300

    Add files via upload

---
+++ ex010.py
@@ -0,0 +1,3 @@
+rs = float(input('qual valor voce tem em reais?'))
+dol = rs/3.27
+print('voce pode comprar {:.3f} dolares'.format(dol))
---
+++ ex011.py
@@ -0,0 +1,6 @@
+a = float(input('qual a altura, em metros, da sua parede?'))
+l = float(input('qual a largura, em metros, da sua parede?'))
+area = a*l
+quant = area/2
+print('a area da sua parede e de: {} metros quadrados'.format(area))
+print('voce precisara de {} litros de tinta '.format(quant))
