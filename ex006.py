2e4452ac0776436952717f611baac7af936a25f9
abnermeroti <109527595+abnermeroti@users.noreply.github.com>
2022-10-11 17:12:00 -0300

    Delete ex006.py

--- ex006.py
+++
@@ -1,5 +0,0 @@
-n = int(input('digite um numero'))
-dobro = n*2
-triplo = n*3
-raiz = n ** (1/2)
-print('seu dobro sera: {}\n seu triplo sera: {}\n sua raiz quadrada sera: {:.2f}'.format(dobro, triplo, raiz))
