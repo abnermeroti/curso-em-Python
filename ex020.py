15defe6bb405dd07b90c1a9f6bd9475e077061e6
abnermeroti <109527595+abnermeroti@users.noreply.github.com>
2022-10-11 17:58:41 -0300

    Delete ex020.py

--- ex020.py
+++
@@ -1,9 +0,0 @@
-from random import shuffle
-a1 = input('digite o nome do aluno1: ')
-a2 = input('digite o nome do aluno2: ')
-a3 = input('digite o nome do aluno3: ')
-a4 = input('digite o nome do aluno4: ')
-lista = [a1, a2, a3, a4]
-shuffle(lista)
-print('a ordem sera: ')
-print(lista)
