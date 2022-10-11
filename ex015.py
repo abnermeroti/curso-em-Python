8793e2cc800da00e4de75a5cdbe583557cd92988
abnermeroti <109527595+abnermeroti@users.noreply.github.com>
2022-10-11 17:58:07 -0300

    Delete ex015.py

--- ex015.py
+++
@@ -1,6 +0,0 @@
-km = float(input('quantos km o carro percorreu?'))
-d = int(input('por quantos dias ele foi alugado?'))
-pd = d*60
-pkm = km*0.15
-pf = pkm + pd
-print('voce pagou R${:.2f} pelo uso do carro'.format(pf))
