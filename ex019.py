ef7240eb56abe27af35d9964967058874e189a7f
abnermeroti <109527595+abnermeroti@users.noreply.github.com>
2022-10-11 17:58:32 -0300

    Delete ex019.py

--- ex019.py
+++
@@ -1,8 +0,0 @@
-from random import choice
-a1 = input('digite o nome do aluno1: ')
-a2 = input('digite o nome do aluno2: ')
-a3 = input('digite o nome do aluno3: ')
-a4 = input('digite o nome do aluno4: ')
-lista = [a1, a2, a3, a4]
-sorteado = choice(lista)
-print('o aluno escolhido foi: {}'.format(sorteado))
