e8007ed25c2a4fe94df25cf5486836122f4df266
abnermeroti <109527595+abnermeroti@users.noreply.github.com>
2022-10-11 17:56:42 -0300

    Delete ex008.py

--- ex008.py
+++
@@ -1,4 +0,0 @@
-m = float(input('digite um valor em metros pra saber seu valor em centimetro e milimetro'))
-cent = m*100
-mm = m*1000
-print('em centimetros: {}\n em milimetros: {}'.format(cent, mm))
