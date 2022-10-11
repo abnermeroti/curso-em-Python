df11abb9cef59a05fe6e08c6a2d1ac2ee1825eba
abnermeroti <109527595+abnermeroti@users.noreply.github.com>
2022-10-11 17:58:15 -0300

    Delete ex016.py

--- ex016.py
+++
@@ -1,6 +0,0 @@
-'''rom math import trunc
-num = float(input('digite um valor: '))
-print('o valor foi {} e sua porcao inteira e {}'.format(num, trunc(num)))'''
-
-num = float(input('digite um numero: '))
-print('o valor digitado foi {} e sua porcao inteira e {}'.format(num, int(num)))
