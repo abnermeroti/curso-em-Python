7b005660c4c8c47576b8cf2c8254d0b328280120
abnermeroti <109527595+abnermeroti@users.noreply.github.com>
2022-10-11 17:11:32 -0300

    Delete ex003.py

--- ex003.py
+++
@@ -1,8 +0,0 @@
-n1 = int(input('digite um numero:'))
-n2 = int(input('digite outro numero:'))
-s = n1 + n2
-#print('o valor da soma entre',n1,'e',n2,':{}'.format(s))
-print('a noma entre {} e {} vale {}'.format(n1, n2, s))
-
-
-
